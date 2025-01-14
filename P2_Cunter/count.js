0e3d602fa83a61cf3935878d5a601ac3dbd4713e
Saimun Al Bashar <hasan.myoffice1@gmail.com>
2025-01-14 17:09:55 +0600

    Counter Project

---
+++ P2_Cunter/count.js
@@ -0,0 +1,30 @@
+// set initial count
+let count = 0;
+
+//select value and button
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(item){
+    // console.log(item)
+    item.addEventListener("click",  function(e){
+       const styles = e.currentTarget.classList;
+       if (styles.contains("decrease")){
+        count--;
+       } else if(styles.contains('increase')) {
+         count++;
+       } else{
+         count = 0;
+       }
+       if(count > 0) {
+         value.style.color = "green";
+       } 
+       if(count < 0){
+        value.style.color = "red";
+       }
+       if(count === 0){
+        value.style.color = "#333";
+       }
+       value.textContent = count;
+    })
+})
