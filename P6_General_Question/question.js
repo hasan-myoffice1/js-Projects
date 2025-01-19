c6785f421f04ed5027a450887d0055a1b2007be1
Saimun Al Bashar <hasan.myoffice1@gmail.com>
2025-01-19 12:13:35 +0600

    Added new code to General Question Project.

--- P6_General_Question/question.js
+++ P6_General_Question/question.js
@@ -1,8 +1,27 @@
-const btns = document.querySelectorAll('.question-btn');
+//using selector inside the element
+const questions = document.querySelectorAll(".question");
 
-btns.forEach(function(btn) {
-    btn.addEventListener("click", function(e){
-       const question = e.currentTarget.parentElement.parentElement;
-       question.classList.toggle('show-text');
+questions.forEach(function(qu){
+    // console.log(qu); 
+    const btn = qu.querySelector('.question-btn');
+    // console.log(btn);
+    btn.addEventListener("click", function() {
+        questions.forEach(function(item){
+            if(item !== qu){
+                item.classList.remove("show-text");
+            }
+        });
+        qu.classList.toggle("show-text");
     })
+    
+    
 })
+
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn) {
+//     btn.addEventListener("click", function(e){
+//        const question = e.currentTarget.parentElement.parentElement;
+//        question.classList.toggle('show-text');
+//     })
+// })
